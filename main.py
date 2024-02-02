e6b232764af2158947bfe1b4d20d6947e6b720c4
AdrianMorneo <116754888+AdrianMorneo@users.noreply.github.com>
2024-02-02 18:49:39 +0100

    Primer commit prueba de conexion

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import redis
+from configparser import ConfigParser
 
+configuracion = ConfigParser()
+configuracion.read("ConexionConfig.ini")
+
+con = redis.StrictRedis(host=configuracion.get('conexion', 'host'), port=configuracion.getint('conexion', 'puerto'), db=configuracion.getint('conexion', 'db'), decode_responses=True)
+
+con.set('nombre', 'Juan')
+con.set('edad', 30)
+
+# Obtener datos de Redis
+nombre = con.get('nombre')
+edad = con.get('edad')
+
+print(f'Nombre: {nombre}')
+print(f'Edad: {edad}')
