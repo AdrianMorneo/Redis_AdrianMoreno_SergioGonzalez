5cdadce6a801c2c0e0c82a720635f38b3024a86f
AdrianMorneo <116754888+AdrianMorneo@users.noreply.github.com>
2024-02-05 16:44:08 +0100

    correcion del metodo de mostrar uno solo

--- Animal.py
+++ Animal.py
@@ -1,7 +1,7 @@
-
 import Conexion as conect
 
 cnt = conect.conectar()
+
 
 def agregarAnimal():
     id = cnt.incr('idAnimal')
@@ -23,24 +23,27 @@ def agregarAnimal():
     cnt.set(f'animal:{animal["id"]}:tipo', animal['tipo'])
     cnt.set(f'animal:{animal["id"]}:nombre', animal['nombre'])
     cnt.set(f'animal:{animal["id"]}:edad', str(animal['edad']))
-    cnt.set(f'animal:{animal["id"]}:apadrinado',str(animal['apadrinado']))
+    cnt.set(f'animal:{animal["id"]}:apadrinado', str(animal['apadrinado']))
     cnt.set(f'animal:{animal["id"]}:dniPadrino', animal['dniPadrino'])
+
     print(f"Animal agregado correctamente con ID: {id}")
 
-def mostrarTodosA():
-    keys = cnt.keys('animal:*')
+def devolverAnimal(id_animal):
+    # Obtener los valores del animal desde Redis
+    tipo = cnt.get(f'animal:{id_animal}:tipo')
+    nombre = cnt.get(f'animal:{id_animal}:nombre')
+    edad = int(cnt.get(f'animal:{id_animal}:edad'))
+    apadrinado_str = cnt.get(f'animal:{id_animal}:apadrinado')
+    apadrinado = apadrinado_str == b'True'
+    dniPadrino = cnt.get(f'animal:{id_animal}:dniPadrino')
 
-    if keys:
-        print("Animales guardados en la base de datos:\n")
-        for key in keys:
-            # Recuperar los valores de cada animal
-            tipo = cnt.get(f'{key}:tipo')
-            nombre = cnt.get(f'{key}:nombre')
-            #edad = int(cnt.get(f'{key}:edad'))
-            #apadrinado = cnt.get(f'{key}:apadrinado')
-            #dniPadrino = cnt.get(f'{key}:dniPadrino')
-
-            # Mostrar la información del animal
-            print(f"Tipo: {tipo}, Nombre: {nombre}")
-    else:
-        print("No hay animales guardados en la base de datos")
+    # Devolver un diccionario con los valores del animal
+    animal = {
+        'id': id_animal,
+        'tipo': tipo,
+        'nombre': nombre,
+        'edad': edad,
+        'apadrinado': apadrinado,
+        'dniPadrino': dniPadrino
+    }
+    return animal
