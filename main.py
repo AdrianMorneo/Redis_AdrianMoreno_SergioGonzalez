6cb3e5d4b2956650dde0bbe25ce00f4396ab03c5
ChevyMaister <sergiogonzalezcorreobasura@gmail.com>
2024-02-09 19:09:34 +0100

    tocado main

--- main.py
+++ main.py
@@ -2,9 +2,28 @@
 import Interfaz as interfaz
 
 import InterfazGrafica as ig
+opcion = ""
 
 
-#ig.mensajeBienvenida()
+while opcion != "1" and opcion != "2" and opcion != "0":
+    print("Bienvenido al santuario de animales de Adrián y Sergio")
+    print("\nSelecciona una opción de interfaz")
+    print("\n1. Interfaz de Consola")
+    print("2. Interfaz Grafica")
+    print("0. Salir sin entrar en ninguna interfaz")
+    opcion = input("\nIntroduce una opción")
+    if opcion == "1":
+        interfaz.ejecutar()
+    elif opcion == "2":
+        ig.mensajeBienvenida()
+        print("Abre la ventana gráfica")
+    elif opcion == "0":
+        print("Saliste de la Aplicación")
+    else:
+        print("No has seleccionado una opcion correcta")
 
-interfaz.ejecutar()
+
+
+
+
 
