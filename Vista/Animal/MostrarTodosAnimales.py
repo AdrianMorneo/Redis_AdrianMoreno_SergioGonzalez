01dcbeecdd2cbc634c3e25eae204b139490ff82f
Sergio Gonzalez <gonmonsergio@gmail.com>
2024-02-13 23:42:22 +0100

    hECHO MOSTRAR TODOS ANIMALES GRAFICOS

--- Vista/Animal/MostrarTodosAnimales.py
+++ Vista/Animal/MostrarTodosAnimales.py
@@ -3,21 +3,27 @@ from PySide2.QtWidgets import QMainWindow, QVBoxLayout, QLabel, QPushButton, QWi
     QHeaderView, QMessageBox
 import EstiloCSS as css
 import ColaboradorConsola as cc
+import Conexion as conect
 
+cnt = conect.conectar()
 
+# Variable global para almacenar la ventana de mostrar animales
+mostrarAnimales = None
+
+# Función para manejar el evento de volver al menú principal
 def handleVuelveSubmenuClick():
-    # Función para manejar el evento de volver al menú principal
     mostrarAnimales.close()
 
-
+# Función para mostrar la ventana de todos los animales
 def mostrarAnimalesWindows():
-#    # Función para mostrar la ventana de todos los colaboradores
-    if cc.mostrarTodos():
-        # Verificar si hay colaboradores en la base de datos
-        global mostrarAnimales
-        # Crear una nueva ventana para mostrar los colaboradores
+    global mostrarAnimales
+
+    animales_info = mostrarTodos()  # Obtener la información de todos los animales
+
+    if animales_info:
+        # Crear la ventana para mostrar los animales
         mostrarAnimales = QMainWindow()
-        mostrarAnimales.setWindowTitle('Mostrar Todos Colaboradores')
+        mostrarAnimales.setWindowTitle('Mostrar Todos los Animales')
         mostrarAnimales.setGeometry(200, 200, 800, 400)
 
         # Aplicar estilo a la ventana utilizando CSS
@@ -31,40 +37,22 @@ def mostrarAnimalesWindows():
         # Crear un layout vertical para organizar los elementos de la ventana
         layout = QVBoxLayout(central_widget)
 
-        # Crear la tabla para mostrar los colaboradores
+        # Crear la tabla para mostrar los animales
         table = QTableWidget()
-
-        # Definir los nombres de las columnas de la tabla
         column_names = ["Tipo", "Nombre", "Edad", "Apadrinado por"]
-        table.setColumnCount(len(column_names))  # Establecer el número de columnas
-        table.setHorizontalHeaderLabels(column_names)  # Establecer los nombres de las columnas
+        table.setColumnCount(len(column_names))
+        table.setHorizontalHeaderLabels(column_names)
 
-        # Obtener la información de los colaboradores desde la base de datos
-        animales_info = cc.mostrarTodos()
+        # Establecer el número de filas de la tabla
+        table.setRowCount(len(animales_info))
 
-        # Agregar los datos de los colaboradores a la tabla
-        '''
-        if animales_info:
-            table.setRowCount(len(animales_info))
-            for row, animal in enumerate(animales_info):
-                table.setItem(row, 0, QTableWidgetItem(animal['tipo']))
-                table.setItem(row, 1, QTableWidgetItem(animal['nombre']))
-                table.setItem(row, 2, QTableWidgetItem(animal['edad']))
-                table.setItem(row, 3, QTableWidgetItem(animal['apadrinado']))
+        # Agregar los datos de los animales a la tabla
+        for row, animal_id in enumerate(animales_info):
+            animal = animales_info[animal_id]
+            for col, valor in enumerate(animal.values()):
+                table.setItem(row, col, QTableWidgetItem(valor))
 
-        else:
-            # Si no hay datos de animal, mostrar un mensaje en la tabla
-            table.setRowCount(1)
-            no_data_item = QTableWidgetItem("No hay animal registrado.")
-            table.setItem(0, 0, no_data_item)
-            table.setColumnSpan(0, 0, len(column_names))  # Fusionar todas las columnas
-        
-        '''
-        # Ajustar el tamaño de las columnas para que se ajusten al contenido
-        table.resizeColumnsToContents()
-
-        # Establecer el ajuste horizontal de la tabla
-        table.horizontalHeader().setStretchLastSection(True)
+        # Ajustar el tamaño de las columnas para que se ajusten todas iguales
         table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
 
         # Botón para volver al menú principal
@@ -75,15 +63,26 @@ def mostrarAnimalesWindows():
         layout.addWidget(table)
         layout.addWidget(botonVolver)
 
-        # Mostrar la ventana de todos los animal
+        # Mostrar la ventana de todos los animales
         mostrarAnimales.show()
-
-"""
     else:
-        # Si no hay animal en la base de datos, mostrar un mensaje de aviso
+        # Si no hay animales en la base de datos, mostrar un mensaje de aviso
         mensaje = QMessageBox()
         mensaje.setWindowTitle("Aviso")
-        mensaje.setText("No hay animal en la BBDD.")
+        mensaje.setText("No hay animales en la BBDD.")
         mensaje.setIcon(QMessageBox.Information)
         mensaje.exec_()
-"""
+def mostrarTodos():
+    animales = {}
+    keys = cnt.keys('A*')
+    if keys:
+        for key in keys:
+            valor = cnt.get(key)
+            detalles = valor.split('\n')
+            detalles_dict = {}
+            for detalle in detalles:
+                if ':' in detalle:  # Verifica si la línea contiene el separador ':'
+                    parametro, valor = detalle.split(': ', 1)  # Limita la división a solo 1 vez
+                    detalles_dict[parametro.strip()] = valor.strip()  # Elimina los espacios en blanco alrededor de los valores
+            animales[key] = detalles_dict
+    return animales
